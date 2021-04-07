36fc1d38938af9abbe40187386a56019f6e9a829
JakeLaCombe <jake.lacombe2@gmail.com>
2021-04-07 00:08:10 -0500

    Implement Initial Scene

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const fs = require("fs");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const appDirectory = fs.realpathSync(process.cwd());
+
+module.exports = {
+    entry: path.resolve(appDirectory, "src/app.ts"), //path to the main .ts file
+    output: {
+        filename: "js/bundleName.js", //name for the javascript file that is created/compiled in memory
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: path.resolve(appDirectory, "public/index.html"),
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    mode: "development",
+};
